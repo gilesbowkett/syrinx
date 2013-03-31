a52b3440fd672724ff9f72338e1b3063ed19c033
Giles Bowkett <giles.bowkett@gmail.com>
2013-03-31 16:07:25 -0700

    renamed to array_similarity

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -1,13 +1,4 @@
-require_relative '../lib/wheatley_arrays'
-
-# FIXME: this is from my powerful but messy JavaScript repetition
-# detector and I guess "robot refactoring assistant" Wheatley.
-# it's written in Ruby and it can do some very primitive automated
-# refactoring of JavaScript, and some very powerful analysis of
-# JavaScript too. it's kind of like a space ship made out of spare
-# parts, and held together with duct tape and bubblegum. anyway,
-# all this Array stuff should probably become ArrayComparator or
-# RecognizesSimilarityInArrays or some such civilized thing.
+require_relative '../lib/array_similarity'
 
 describe Array do
 
