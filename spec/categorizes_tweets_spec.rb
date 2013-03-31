c43e4c1a792c81cafc18f98171148e68ffe3725d
Giles Bowkett <giles.bowkett@gmail.com>
2013-03-31 12:34:11 -0700

    set overlap spec, plus some cleanup

--- spec/categorizes_tweets_spec.rb
+++ spec/categorizes_tweets_spec.rb
@@ -1,10 +1,9 @@
 require_relative "../lib/categorizes_tweets"
 require_relative "../lib/imported_tweet"
 
-# I accidentally an integration spec
 describe CategorizesTweets do
 
-  before do # I bet you didn't see that one coming
+  before do
 
     @ruby_tweet = ImportedTweet.create([
       '318169735143493632',
@@ -36,6 +35,20 @@ describe CategorizesTweets do
   it "works against mulitple tweets" do
     categorized_tweets = @categorizes_tweets.categorize!(@ruby_tweet, @music_tweet)
     categorized_tweets.should == {:ruby => [@ruby_tweet], :music => [@music_tweet]}
+  end
+
+  # this kind of redundancy doesn't bother me. if I go to Twitter because I'm curious
+  # about Ruby news, and I see news about Ruby and music, that's cool with me. same
+  # thing if I wanted Ruby news. so for now this is fine.
+  it "permits set overlap" do
+    ruby_music_tweet = ImportedTweet.create([
+      '318169735143493632',
+      '2013-03-31 01:15:50 +0000',
+      'gilesgoatboy',
+      'using ruby to make music!'
+    ])
+    categorized_tweets = @categorizes_tweets.categorize!(ruby_music_tweet)
+    categorized_tweets.should == {:ruby => [ruby_music_tweet], :music => [ruby_music_tweet]}
   end
 
 end
