b4193c0457cd1e286138f4f797c30c6c8b2badf7
Giles Bowkett <gilesb@gmail.com>
2013-04-02 13:29:51 -0700

    LinkFilter outsmarts crappy punctuation! huzzah

--- spec/link_filter_spec.rb
+++ spec/link_filter_spec.rb
@@ -15,6 +15,11 @@ describe LinkFilter do
     LinkFilter.extract_link(@tweet.text).should == 'http://t.co/1KXSm9ED1G'
   end
 
+  it "skips unnecessary punctuation" do
+    @tweet.text = 'Story Tape (http://t.co/1KXSm9ED1G)'
+    LinkFilter.extract_link(@tweet.text).should == 'http://t.co/1KXSm9ED1G'
+  end
+
   it "survives random remarks" do
     @tweet.text = "marbles"
     LinkFilter.extract_link(@tweet.text).should be_nil
