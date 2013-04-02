29550b3dab8ef51ec16b1bf8171ab6f7ba49e19a
Giles Bowkett <gilesb@gmail.com>
2013-04-02 12:27:54 -0700

    moved new Category object/spec to their own files

--- lib/categorizes_tweets.rb
+++ lib/categorizes_tweets.rb
@@ -1,14 +1,3 @@
-class Category < Struct.new(:name, :qualifying_terms)
-
-  def include?(tweet)
-    qualifying_terms.each do |qualifying_term|
-      return true if tweet.text.include? qualifying_term
-    end
-    return false
-  end
-
-end
-
 class CategorizesTweets
   attr_accessor :categories, :categorized
   def initialize
---
+++ spec/category_spec.rb
@@ -0,0 +1,24 @@
+require_relative "../lib/category"
+require_relative "../lib/tweet"
+
+describe Category do
+  before do
+    @ruby = Category.new(:ruby, ["ruby"])
+    @music = Category.new(:music, ["music"])
+    @ruby_tweet = Tweet.new(
+      '318169735143493632',
+      '2013-03-31 01:15:50 +0000',
+      'gilesgoatboy',
+      'ruby ruby ruby!'
+    )
+  end
+
+  it "knows what fits within it" do
+    @ruby.include?(@ruby_tweet).should be_true
+  end
+
+  it "knows what doesn't fit" do
+    @music.include?(@ruby_tweet).should be_false
+  end
+end
+
