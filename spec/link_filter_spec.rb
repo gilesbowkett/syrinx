e17f10f605bc71ca8b3b8964f25f9b50f709b742
Giles Bowkett <gilesb@gmail.com>
2013-04-07 13:10:29 -0700

    spec accuracy

--- spec/link_filter_spec.rb
+++ spec/link_filter_spec.rb
@@ -30,7 +30,7 @@ describe LinkFilter do
     LinkFilter.extract_link(@tweet.text).should be_nil
   end
 
-  it "collects tweets with links" do
+  it "excludes tweets without links" do
     other_tweet = Tweet.new(
       '318713868924903424',
       '2013-04-01 13:18:02 +0000',
