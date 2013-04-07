98b9ad54766afeb87a34de967f3da954babc1118
Giles Bowkett <gilesb@gmail.com>
2013-04-07 13:28:59 -0700

    messy bugfix >.<

--- lib/category.rb
+++ lib/category.rb
@@ -2,7 +2,7 @@ class Category < Struct.new(:name, :qualifying_terms)
 
   def include?(tweet)
     qualifying_terms.each do |qualifying_term|
-      return true if tweet.text.match /#{qualifying_term}/i
+      return true if LinkFilter.remove_link(tweet.text).match /#{qualifying_term}/i
     end
     false
   end
