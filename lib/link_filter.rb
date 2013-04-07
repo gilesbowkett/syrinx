05dbdd448701d8b4d20d875fcc888309d2f3c0ce
Giles Bowkett <gilesb@gmail.com>
2013-04-07 12:44:42 -0700

    refactor to constant (wanting to become instance)

--- lib/link_filter.rb
+++ lib/link_filter.rb
@@ -1,14 +1,15 @@
 class LinkFilter
+  FINDS_LINK = /\s*(http:\/\/t.co\S{11})/
 
   def self.extract_link(text)
-    url_match = text.match(/\s*(http:\/\/t.co\S{11})/)
+    url_match = text.match(FINDS_LINK)
     if url_match
       url_match[1]
     end
   end
 
   def self.remove_link(text)
-    text.gsub(/\s*(http:\/\/t.co\S{11})/, '')
+    text.gsub(FINDS_LINK, '')
   end
 
   def self.collect_tweets_with_links(tweets)
